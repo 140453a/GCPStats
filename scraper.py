bd86bc48aa55530f4062061738deb2ffbd63afe8
140453a <34776660+140453a@users.noreply.github.com>
2019-04-26 10:12:45 -0300

    Comments, made x start at 1, fixed depreciated func

--- scraper.py
+++ scraper.py
@@ -5,12 +5,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+## Rss feed containing the podcast.
 feed = feedparser.parse("https://audioboom.com/channels/4072907.rss")
 
 outer_list = []
 
 for entry in feed['entries']:
-    if re.search("^.*Cannon", entry.title) is None:
+    if re.search("^.*Cannon", entry.title) is None: ## Get rid of cannon fodder
         outer_list.append((entry.title, entry.itunes_duration))
 
 times = []
@@ -19,16 +20,16 @@ for x in outer_list[::-1]: # Iterate backwards from oldest to most recent
     times.append(round(float(x[1]) / 60 ))
     names.append(x[0])
 
-print(times)
-
 data_set = list(zip(names, times))
 df = pd.DataFrame(data = data_set, columns=['Episode', 'Episode Length'])
+df.index = np.arange(1, len(df)+1) ## Make x start at 1
 df['Moving Average (8)'] = df['Episode Length'].rolling(8).mean()
 df['Average'] = df['Episode Length'].mean()
 
 lines = df.plot.line(title='GCP Episodes and Running Time (' + str(datetime.datetime.now().date()) + ')')
 lines.set_xlabel("Episode #")
 lines.set_ylabel("Running Time (Minutes)")
+lines.set_ylim(bottom=0) ## Make y axis start at 0
 
 
-plt.show()
+plt.show() ## Show plot!
