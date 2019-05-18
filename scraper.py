b402a1512f59ea80e93f91ed8735c74669b4f714
140453a <34776660+140453a@users.noreply.github.com>
2019-05-17 22:48:43 -0300

    Lumped multi-part episodes together, labeled average, deleted duplicate episodes
    The RSS feed had sneaky duplicate episodes in it, I didn't realize... As per a suggestion in the reddit thread, multi-part episodes are now lumped into one episode.

--- scraper.py
+++ scraper.py
@@ -5,31 +5,44 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-## Rss feed containing the podcast.
 feed = feedparser.parse("https://audioboom.com/channels/4072907.rss")
 
 outer_list = []
-
+prev = [0]
 for entry in feed['entries']:
-    if re.search("^.*Cannon", entry.title) is None: ## Get rid of cannon fodder
-        outer_list.append((entry.title, entry.itunes_duration))
+    if re.search("^.*Cannon", entry.title) is None:
+            curr = [int(s) for s in entry.title.split() if s.isdigit()]
+            if curr[0] == prev[0]: # multi-episode
+                if re.search("part|Part", entry.title) is None: # duplicate
+                    continue
+                saved = outer_list.pop()
+                new_duration = int(entry.itunes_duration) + int(saved[1])
+                outer_list.append((entry.title, new_duration))
+                prev = curr
+                continue
+            prev = curr
+
+            outer_list.append((entry.title, entry.itunes_duration))
+
 
 times = []
 names = []
 for x in outer_list[::-1]: # Iterate backwards from oldest to most recent
     times.append(round(float(x[1]) / 60 ))
     names.append(x[0])
+    print(x)
+
+
 
 data_set = list(zip(names, times))
 df = pd.DataFrame(data = data_set, columns=['Episode', 'Episode Length'])
-df.index = np.arange(1, len(df)+1) ## Make x start at 1
 df['Moving Average (8)'] = df['Episode Length'].rolling(8).mean()
-df['Average'] = df['Episode Length'].mean()
+avg = df['Episode Length'].mean()
+df['Average(' + str(avg)[:4] + ')'] = avg
 
 lines = df.plot.line(title='GCP Episodes and Running Time (' + str(datetime.datetime.now().date()) + ')')
 lines.set_xlabel("Episode #")
 lines.set_ylabel("Running Time (Minutes)")
-lines.set_ylim(bottom=0) ## Make y axis start at 0
 
 
-plt.show() ## Show plot!
+plt.show()
